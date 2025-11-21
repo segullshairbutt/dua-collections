9abdc6c5c7efa5d98e3a2659e32310ab256623af
gulshairbutt <segullshairbutt@gmail.com>
2025-11-21 14:13:50 +0100

    htmx-transformation
    Removes the nuxt.js implementaiton

--- src/app/components/Navbar/routes.ts
+++
@@ -1,8 +0,0 @@
-export const TAGS_ROUTES = [
-  { name: 'All', url: { pathname: '/Duas' } },
-  { name: 'Quranic Duas', url: { pathname: '/Duas', query: { tag: 'Quran' } } },
-  { name: 'Masnoon Duas', url: { pathname: '/Duas', query: { tag: 'Masnoon' } } },
-  { name: 'Sleeping', url: { pathname: '/Duas', query: { tag: 'Sleep' } } },
-  { name: 'Mosque', url: { pathname: '/Duas', query: { tag: 'Mosque' } } },
-  { name: 'House', url: { pathname: '/Duas', query: { tag: 'House' } } },
-];
